6fb70b13c4bc30c1aaaa1ba70fadf776821a12de
Robert Ochoa <ochoa.robert1@gmail.com>
2021-03-11 02:18:42 -0400

    New code for renaming thumbnails

--- functions.php
+++ functions.php
@@ -422,11 +422,59 @@ function get_galleries_by_folders()
 		foreach ($ids as $id) {
 			$json_galleries[] = array(
 				'topic' => $folder['name'],
-				'thumb_nail' => wp_get_attachment_image_url($id['attachment_id'], array('150', '99'), false),
+				'thumb_nail' => wp_get_attachment_image_url($id['attachment_id'], 'thumbnail', false),
 				'full_image' => wp_get_attachment_image_url($id['attachment_id'], 'full', false)
 			);
 		}
 	}
 
 	return $json_galleries;
+}
+
+// The filter runs when resizing an image to make a thumbnail or intermediate size.
+add_filter('image_make_intermediate_size', 'wpse_123240_rename_intermediates');
+function wpse_123240_rename_intermediates($image)
+{
+	// Split the $image path into directory/extension/name
+	$info = pathinfo($image);
+	$dir = $info['dirname'] . '/';
+	$ext = '.' . $info['extension'];
+	$name = wp_basename($image, "$ext");
+
+	// Get image information 
+	// Image edtor is used for this
+	$img = wp_get_image_editor($image);
+	// Get image size, width and height
+	$img_size = $img->get_size();
+
+	// Image prefix for builtin sizes
+	// Note: beware possible conflict with custom images sizes with the same width
+	$widths = [];
+	$size_based_img_name_prefix = '';
+	foreach (get_intermediate_image_sizes() as $_size) {
+		if (in_array($_size, ['thumbnail', 'medium', 'medium_large', 'large'])) {
+			$width = get_option("{$_size}_size_w");
+			if (!isset($widths[$width])) {
+				$widths[$width]  = $_size;
+			}
+		}
+	}
+	if (array_key_exists($img_size['width'], $widths)) {
+		$size_based_img_name_prefix = '-' . $widths[$img_size['width']];
+		$name_prefix = substr($name, 0, strrpos($name, '-'));
+	} else {
+		$name_prefix = $name;
+	}
+
+	// Build our new image name
+	$new_name = $dir . $name_prefix . $size_based_img_name_prefix . $ext;
+
+	// Rename the intermediate size
+	$did_it = rename($image, $new_name);
+
+	// Renaming successful, return new name
+	if ($did_it)
+		return $new_name;
+
+	return $image;
 }
